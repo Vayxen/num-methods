12ad1f54a8626e18ecf82a454f435f2d34295d16
Vayxen <vittostrano3302@gmail.com>
2023-11-15 19:07:12 +0100

    renamed variable for clarity

--- Python Codes/3a-trapeziumNC.py
+++ Python Codes/3a-trapeziumNC.py
@@ -6,9 +6,9 @@ fp = lambda x: 2* f(x) * (1 + 2*(x**2))
 h = 0.2 #step
 result = f(0) + f(0.8) #implied: a = 0, b= 0.8
 error = ((h**2)/12) * 0.8 * fp(0.8)
-arg = np.linspace(h, 0.8 - h, int(0.8/h)) #this could use more numbers but works in a general case as b-a/h is the num of intervals, whereas the sum needs to stop at the n-th term, which is the second to last step (ergo b - h)
+argument = np.linspace(h, 0.8 - h, int(0.8/h)) #this could use more numbers but works in a general case as b-a/h is the num of intervals, whereas the sum needs to stop at the n-th term, which is the second to last step (ergo b - h)
 
-for i in arg:
+for i in argument:
     result += f(i)
 
 result = (h/2)*result
